cd0bbbd5d62694ea736acec882463c3d990727c7
Souk159 <soukthunva152@gmail.com>
2026-05-28 20:34:08 +0700

    fix bugs

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
   images: {
-    remotePatterns: [
-      { protocol: "http", hostname: "localhost" },
-      { protocol: "https", hostname: "**" },
-    ],
+    unoptimized: true,
   },
 };
 
